ac82196786ac70f7941997687ede0953651bb812
Hewson Schulz <hewsonschulz@gmail.com>
2024-03-04 14:51:26 -0600

    file structure, and create-react-app initialization

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css"
+import { ApplicationViews } from "./components/views/ApplicationViews"
+
+export const App = () => {
+  return (
+    <div className="app-container">
+      <ApplicationViews />
+    </div>
+  )
+}
