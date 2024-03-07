160a20b8a50b8d23fadb008f131b1512e364cd81
Hewson Schulz <hewsonschulz@gmail.com>
2024-03-07 13:47:48 -0600

    route authorization functionality

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
-import { Route, Routes } from "react-router-dom"
+import { Outlet, Route, Routes } from "react-router-dom"
 import { Login } from "../auth/Login"
 import { Register } from "../auth/Register"
+import { AuthorizedRoute } from "../auth/AuthorizedRoute"
 
 export const ApplicationViews = () => {
   const [loggedInUser, setLoggedInUser] = useState(null)
@@ -20,8 +21,34 @@ export const ApplicationViews = () => {
 
   return (
     <Routes>
-      <Route path="/login" element={<Login setLoggedInUser={setLoggedInUser} />} />
-      <Route path="/register" element={<Register setLoggedInUser={setLoggedInUser} />} />
+      <Route
+        path="/"
+        element={
+          <AuthorizedRoute loggedInUser={loggedInUser}>
+            <Outlet />
+          </AuthorizedRoute>
+        }>
+        <Route index element={<>[insert home page here]</>} />
+
+        {/*//* add more application routes here */}
+      </Route>
+
+      <Route
+        path="/login"
+        element={
+          <AuthorizedRoute loggedInUser={loggedInUser} isPublicOnly={true}>
+            <Login setLoggedInUser={setLoggedInUser} />
+          </AuthorizedRoute>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <AuthorizedRoute loggedInUser={loggedInUser} isPublicOnly={true}>
+            <Register setLoggedInUser={setLoggedInUser} />
+          </AuthorizedRoute>
+        }
+      />
     </Routes>
   )
 }
