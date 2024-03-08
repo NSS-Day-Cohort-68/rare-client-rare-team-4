cb09de5e45b3ef334c731c800f2d7f3ce4d11ddc
Dale Dear <kristendaledear@gmail.com>
2024-03-08 11:03:29 -0600

    commit changes to pull latest main

--- src/components/lists/CategoryList.js
+++ src/components/lists/CategoryList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllCategories } from '../../managers/categoryManager';
 
@@ -6,7 +6,11 @@ import { getAllCategories } from '../../managers/categoryManager';
 export const CategoryList = () => {
     const [allCategories, setAllCategories] = useState([])
   
-  
+    useEffect(() => {
+      getAllCategories()
+        .then(categories => setAllCategories(categories))
+        .catch(error => console.error('Error fetching categories:', error))
+  }, [])
   return (
     <div>
       <h1>Category List Page</h1>
@@ -14,7 +18,7 @@ export const CategoryList = () => {
       
       <h2>Existing Categories:</h2>
       <ul>
-        {testCategories.map(category => (
+        {allCategories.map(category => (
           <li key={category.id}>
             {category.label}
           </li>
