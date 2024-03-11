821fec0ca48dddf67bdf4a448c3d9e1e57fbba12
Hewson Schulz <hewsonschulz@gmail.com>
2024-03-11 14:18:51 -0500

    added links to post details

--- src/components/views/posts/PostsList.js
+++ src/components/views/posts/PostsList.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getAllPosts } from "../../../managers/postManager"
+import { Link } from "react-router-dom"
 import "./Post.css"
 
 export const removeBadPosts = (posts) => {
@@ -27,15 +28,17 @@ export const PostsList = () => {
         <h1 className="posts-list__header">All Posts</h1>
         {filteredPosts.length > 0 ? (
           filteredPosts.map((post) => (
-            <ul key={post.id} className="post__container">
-              <div className="post__content-a">
-                <li className="post__title">{post.title}</li>
-                <li className="post__category">{post.category.label}</li>
-              </div>
-              <li className="post__username">
-                By <i className="post__username-name">{post.user.username}</i>
-              </li>
-            </ul>
+            <Link to={`/post-details/${post.id}`} className="post__link">
+              <ul key={post.id} className="post__container">
+                <div className="post__content-a">
+                  <li className="post__title">{post.title}</li>
+                  <li className="post__category">{post.category.label}</li>
+                </div>
+                <li className="post__username">
+                  By <i className="post__username-name">{post.user.username}</i>
+                </li>
+              </ul>
+            </Link>
           ))
         ) : (
           <div className="posts-list__message">No posts have been created yet.</div>
--- src/components/views/posts/UserPosts.js
+++ src/components/views/posts/UserPosts.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getAllPosts } from "../../../managers/postManager.js"
 import { removeBadPosts } from "./PostsList.js"
+import { Link } from "react-router-dom"
 import "./Post.css"
 
 export const UserPostList = ({ loggedInUser }) => {
@@ -27,15 +28,17 @@ export const UserPostList = ({ loggedInUser }) => {
         <h1 className="posts-list__header">My Posts</h1>
         {filteredPosts.length > 0 ? (
           filteredPosts.map((post) => (
-            <ul key={post.id} className="post__container">
-              <div className="post__content-a">
-                <li className="post__title">{post.title}</li>
-                <li className="post__category">{post.category.label}</li>
-              </div>
-              <li className="post__username">
-                By <i className="post__username-name">{post.user.username}</i>
-              </li>
-            </ul>
+            <Link to={`/post-details/${post.id}`} className="post__link">
+              <ul key={post.id} className="post__container">
+                <div className="post__content-a">
+                  <li className="post__title">{post.title}</li>
+                  <li className="post__category">{post.category.label}</li>
+                </div>
+                <li className="post__username">
+                  By <i className="post__username-name">{post.user.username}</i>
+                </li>
+              </ul>
+            </Link>
           ))
         ) : (
           <div className="posts-list__message">No posts found for current user.</div>
