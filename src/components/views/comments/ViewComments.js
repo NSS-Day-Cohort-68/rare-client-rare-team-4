600bba51246c786902c4947f4c85c85b4cf48333
Kimberly Summers <kasummers16@gmail.com>
2024-03-15 15:35:08 -0500

    finished view comments

--- src/components/views/comments/ViewComments.js
+++ src/components/views/comments/ViewComments.js
@@ -1,32 +1,50 @@
 import { useEffect, useState } from "react"
 import { getAllComments } from "../../../managers/commentManager.js"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import { getPostById } from "../../../managers/postManager.js"
 import "./comments.css"
 
 export const ViewComments = () => {
+  const [postTitle, setPostTitle] = useState("")
   const [postComments, setPostComments] = useState([])
   const { postId } = useParams()
+
+  const parsedPostId = parseInt(postId)
+
+  useEffect(() => {
+    getPostById(parsedPostId).then((post) => {
+      if (post && post.id === parsedPostId) {
+        setPostTitle(post.title)
+      }
+    })
+  }, [postId])
 
   useEffect(() => {
     getAllComments().then((commentArray) => {
       const currentComments = commentArray.filter(
         (comment) => comment.post_id === parseInt(postId)
       )
-      setPostComments(currentComments)
+      setPostComments(currentComments.reverse())
     })
   }, [postId])
 
   return (
     <>
-      <h2 className="viewCommentsTitle">Comments for {postId}</h2>
-      <section className="postComments">
+      <h2 className="viewCommentsTitle">Comments for {postTitle}</h2>
+      <section className="return-btn">
+        <Link to={`/post-details/${postId}`}>
+          <button>Return to Post</button>
+        </Link>
+      </section>
+      <section>
         {postComments.length === 0 ? (
           <p>No comments available.</p>
         ) : (
           postComments.map((comment) => (
-            <div className="comments" key={comment.id}>
+            <div className="postComments" key={comment.id}>
               <div className="commentAuthorDate">
-                Written by: {comment.user.first_name} on {comment.date}
+                Written by: {comment.user.first_name} {comment.user.last_name}{" "}
+                on {comment.date}
               </div>
               <div className="commentContent">{comment.content}</div>
             </div>
